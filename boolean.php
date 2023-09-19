24eb21e328d016e41c487bcc9acc4134988d08ba
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2023-09-19 19:18:15 +0900

    Tipe Data Boolean

---
+++ boolean.php
@@ -0,0 +1,26 @@
+<?php
+    $x = true; // 1 Nilai dari Boolean
+    $y = false; //0
+
+    // Operasi Boolean AND = x dan OR = +
+    $and = $x && $y && true || true;
+
+    var_dump($and);
+
+    $or = $x || $y;
+
+    var_dump($or)
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipe Data Boolean</title>
+</head>
+<body>
+    
+</body>
+</html>
